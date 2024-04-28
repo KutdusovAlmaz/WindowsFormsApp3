9ae0134cd2667c42cb72cd8eaf9d5ab7e6275923
user <user@WIN-B9Q80GRCCEO>
2024-04-28 21:03:31 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.cmbMaterial = new System.Windows.Forms.ComboBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(49, 13);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 0;
+            this.txtWidth.Text = "Ширина (см)";
+            this.txtWidth.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // cmbMaterial
+            // 
+            this.cmbMaterial.FormattingEnabled = true;
+            this.cmbMaterial.Items.AddRange(new object[] {
+            " \"Пластик\", \"Алюминий\", \"Соломка\", \"Текстиль\" "});
+            this.cmbMaterial.Location = new System.Drawing.Point(81, 114);
+            this.cmbMaterial.Name = "cmbMaterial";
+            this.cmbMaterial.Size = new System.Drawing.Size(121, 21);
+            this.cmbMaterial.TabIndex = 1;
+            this.cmbMaterial.Text = "Расчитать стоимость";
+            this.cmbMaterial.SelectedIndexChanged += new System.EventHandler(this.btnCalculate_SelectedIndexChanged);
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(155, 12);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 2;
+            this.txtHeight.Text = "Высота (см)";
+            this.txtHeight.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(91, 211);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(247, 49);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Расчитать стоимость";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.cmbMaterial_Click);
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(329, 12);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(100, 20);
+            this.txtQuantity.TabIndex = 4;
+            this.txtQuantity.Text = "Количество";
+            this.txtQuantity.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.cmbMaterial);
+            this.Controls.Add(this.txtWidth);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.ComboBox cmbMaterial;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbMaterial.SelectedIndex = 0;
+            txtWidth.Focus();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbMaterial_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int width = int.Parse(txtWidth.Text.Trim());
+                int height = int.Parse(txtHeight.Text.Trim());
+                int quantity = int.Parse(txtQuantity.Text.Trim());
+                string material = cmbMaterial.SelectedItem.ToString();
+
+                double price = CalculatePrice(width, height, material);
+                double total = price * quantity;
+                MessageBox.Show($"Общая стоимость: {total} рублей.", "Жалюзи");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private double CalculatePrice(int width, int height, string material)
+        {
+            double price = 0;
+
+            switch (material)
+            {
+                case "Пластик":
+                    price = 50 * (width + height);
+                    break;
+                case "Алюминий":
+                    price = 100 * (width + height);
+                    break;
+                case "Соломка":
+                    price = 30 * (width + height);
+                    break;
+                case "Текстиль":
+                    price = 70 * (width + height);
+                    break;
+            }
+
+            return price;
+        }
+    }
+}
